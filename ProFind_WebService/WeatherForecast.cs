f603360f4ae8bd0c96de4ea429d0afb3875934bb
Víctor López <vicrenlopez14115@gmal.com>
2022-07-18 16:36:37 -0600

    Add project files.

---
+++ ProFind_WebService/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ProFind_WebService
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
