84960188db71347fbe31e6f89ec7756822c43528
Víctor López <vicrenlopez14115@gmal.com>
2022-07-18 21:43:00 -0600

    Project structure with MMVC.

--- ProFind/MainPage.xaml.cs
+++ ProFind/MainPage.xaml.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -25,6 +31,36 @@ namespace ProFind
         public MainPage()
         {
             this.InitializeComponent();
+
+            ApplicationViewTitleBar formattableTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+            formattableTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            applyAcrylicAccent(MainGrid);
+        }
+
+        private void applyAcrylicAccent(Panel panel)
+        {
+            _compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+            _hostSprite = _compositor.CreateSpriteVisual();
+            _hostSprite.Size = new Vector2((float)panel.ActualWidth, (float)panel.ActualHeight);
+
+            ElementCompositionPreview.SetElementChildVisual(panel, _hostSprite);
+            _hostSprite.Brush = _compositor.CreateHostBackdropBrush();
+        }
+        Compositor _compositor;
+        SpriteVisual _hostSprite;
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (_hostSprite != null)
+                _hostSprite.Size = e.NewSize.ToVector2();
+        }
+
+        private void NavigationView_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
+        {
+
         }
     }
 }
