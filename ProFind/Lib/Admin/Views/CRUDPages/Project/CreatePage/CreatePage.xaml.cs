1da3e36fd059ca8b4894c883e382199ba915d34a
Susan Castillo <susancasoli@gmail.com>
2022-08-24 16:10:44 -0600

    Admin and project done - admin

---
+++ ProFind/Lib/Admin/Views/CRUDPages/Project/CreatePage/CreatePage.xaml.cs
@@ -0,0 +1,130 @@
+﻿using Application.Models;
+using Application.Services;
+using ProFind.Lib.Admin.Controllers;
+using ProFind.Lib.Global.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProFind.Lib.Admin.Views.CRUDPages.Project.CreatePage
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class CreatePage : Page
+    {
+        private PFProject newObject = new PFProject();
+
+        private string[] status = Enum.GetNames(typeof(PFProjectStatus));
+        private List<PFProfessional> professionals = new List<PFProfessional>();
+        private List<string> professionalStrings = new List<string>();
+
+        private List<PFClient> clients = new List<PFClient>();
+        private List<string> clientStrings = new List<string>();
+
+        public CreatePage()
+        {
+            this.InitializeComponent();
+            loadUsefulThings();
+        }
+
+        public async void loadUsefulThings()
+        {
+            (professionals, professionalStrings) = await new PFProfessionalService().GetComboboxChoices();
+            (clients, clientStrings) = await new PfClientService().GetComboboxChoices();
+
+            InitialStatus_cb.ItemsSource = status;
+            Professional_cb.ItemsSource = professionalStrings;
+            Client_cb.ItemsSource = clientStrings;
+        }
+
+
+        private async void Create_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(Title_tb.Text))
+            {
+
+                var dialog = new MessageDialog("The field is empty");
+                await dialog.ShowAsync();
+                return;
+            }
+            else if (string.IsNullOrEmpty(Description_tb.Text))
+            {
+                var dialog = new MessageDialog("The field is empty");
+                await dialog.ShowAsync();
+                return;
+            }
+
+            var result = await new PfProjectService().Create(newObject);
+
+            if (result == System.Net.HttpStatusCode.OK)
+            {
+                new InAppNavigationController().GoBack();
+            }
+
+        }
+
+        private async void PictureSelection_btn_Checked(object sender, RoutedEventArgs e)
+        {
+            PictureSelection_btn.IsChecked = !PictureSelection_btn.IsChecked;
+        }
+
+        private async void PictureSelection_btn_Click(object sender, RoutedEventArgs e)
+        {
+            newObject.PicturePJ = await (await PickFileHelper.PickImage()).ToByteArrayAsync();
+
+        }
+
+        private void Title_tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            newObject.TitlePJ = Title_tb.Text;
+        }
+
+        private void Description_tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            newObject.DescriptionPJ = Description_tb.Text;
+        }
+
+
+
+        private void TotalPrice_tb_ValueChanged(Microsoft.UI.Xaml.Controls.NumberBox sender, Microsoft.UI.Xaml.Controls.NumberBoxValueChangedEventArgs args)
+        {
+            newObject.TotalPricePJ = (float)sender.Value;
+        }
+
+        private void InitialStatus_cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PFProjectStatus selectedStatus = PFProjectStatus.Inactive;
+
+            if (InitialStatus_cb.SelectedIndex == 1)
+                selectedStatus = PFProjectStatus.Active;
+
+            newObject.Status = selectedStatus;
+            newObject.IdPS1 = selectedStatus == PFProjectStatus.Inactive ? "0" : "1";
+        }
+
+        private void Professional_cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            newObject.ResponsibleProfessional = professionals[Professional_cb.SelectedIndex];
+        }
+
+        private void Client_cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            newObject.ResponsibleClient = clients[Client_cb.SelectedIndex];
+        }
+    }
+}
---
+++ ProFind/Lib/Admin/Views/CRUDPages/Project/UpdatePage/UpdatePage.xaml.cs
@@ -0,0 +1,157 @@
+﻿using Application.Models;
+using Application.Services;
+using ProFind.Lib.Admin.Controllers;
+using ProFind.Lib.Admin.Views.ActivityCRUD;
+using ProFind.Lib.Global.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProFind.Lib.Admin.Views.CRUDPages.Project.UpdatePage
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class UpdatePage : Page
+    {
+        PFProject toManipulate = new PFProject();
+        private string[] status = Enum.GetNames(typeof(PFProjectStatus));
+        private List<PFProfessional> professionals = new List<PFProfessional>();
+        private List<string> professionalStrings = new List<string>();
+
+        private List<PFClient> clients = new List<PFClient>();
+        private List<string> clientStrings = new List<string>();
+        
+        public UpdatePage()
+        {
+            this.InitializeComponent();
+        }
+        private async void loadUsefulthings()
+        {
+            (professionals, professionalStrings) = await new PFProfessionalService().GetComboboxChoices();
+            (clients, clientStrings) = await new PfClientService().GetComboboxChoices();
+
+            InitialStatus_cb.ItemsSource = status;
+            Professional_cb.ItemsSource = professionalStrings;
+            Client_cb.ItemsSource = clientStrings;
+
+            SelectedPicture_pp.Source = toManipulate.PicturePJ.ToBitmapImage();
+            Title_tb.Text = toManipulate.TitlePJ ?? "";
+            Description_tb.Text = toManipulate.DescriptionPJ ?? "";
+            TotalPrice_tb.Text = ((int)toManipulate.TotalPricePJ).ToString();
+            InitialStatus_cb.SelectedIndex = ((int)toManipulate.Status) - 1;
+            Professional_cb.SelectedIndex = professionals.FindIndex(x => x.IdP == toManipulate.ResponsibleProfessional.IdP);
+            Client_cb.SelectedIndex = clients.FindIndex(x => x.IdC == toManipulate.ResponsibleClient.IdC);
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            toManipulate = (PFProject)e.Parameter;
+            loadUsefulthings();
+        }
+
+        private async void Reset_btn_Click(object sender, RoutedEventArgs e)
+        {
+            // Reset with the same ID
+            toManipulate = new PFProject()
+            {
+                IdPJ = toManipulate.IdPJ,
+
+            };
+            loadUsefulthings();
+        }
+
+
+        private async void Update_btn_Click(object sender, RoutedEventArgs e)
+        {
+            await new PfProjectService().Update(toManipulate);
+        }
+
+        private async void Delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            await new PfProjectService().Delete(toManipulate.IdPJ);
+        }
+
+        private void Back_btn_Click(object sender, RoutedEventArgs e)
+        {
+            new InAppNavigationController().GoBack();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            toManipulate.PicturePJ = await (await PickFileHelper.PickImage()).ToByteArrayAsync();
+        }
+
+        private void PictureSelection_btn_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void PictureSelection_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var pickedFile = await PickFileHelper.PickImage();
+            toManipulate.PicturePJ = await (pickedFile).ToByteArrayAsync();
+            SelectedPicture_tbk.Text = pickedFile.DisplayName;
+        }
+
+        private void Title_tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            toManipulate.TitlePJ = Title_tb.Text;
+        }
+
+        private void Description_tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            toManipulate.DescriptionPJ = Description_tb.Text;
+        }
+
+        private void TotalPrice_tb_ValueChanged(Microsoft.UI.Xaml.Controls.NumberBox sender, Microsoft.UI.Xaml.Controls.NumberBoxValueChangedEventArgs args)
+        {
+            toManipulate.TotalPricePJ = (int)TotalPrice_tb.Value;
+        }
+
+        private void InitialStatus_cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PFProjectStatus selectedStatus = PFProjectStatus.Inactive;
+
+            if (InitialStatus_cb.SelectedIndex == 1)
+                selectedStatus = PFProjectStatus.Active;
+
+            toManipulate.Status = selectedStatus;
+            toManipulate.IdPS1 = selectedStatus == PFProjectStatus.Inactive ? "0" : "1";
+        }
+
+        private void Professional_cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            toManipulate.ResponsibleProfessional = professionals[Professional_cb.SelectedIndex];
+            toManipulate.IdP1 = professionals[Professional_cb.SelectedIndex].IdP;
+
+        }
+
+        private void Client_cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            toManipulate.ResponsibleClient = clients[Client_cb.SelectedIndex];
+            toManipulate.IdC1 = clients[Client_cb.SelectedIndex].IdC;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            new InAppNavigationController().NavigateTo(typeof(ReadPageActivity), toManipulate);
+        }
+    }
+}
