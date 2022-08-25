90a3db97e220b5bbd151936210c8afeca566f0b7
Susan Castillo <susancasoli@gmail.com>
2022-08-24 18:56:39 -0600

    Notification CRUD done - admin

---
+++ ProFind/Lib/Admin/Views/CRUDPages/Notification/CreatePage/CreatePage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Application.Models;
+using Application.Services;
+using ProFind.Lib.Admin.Controllers;
+using ProFind.Lib.Global.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProFind.Lib.Admin.Views.CRUDPages.Notification.CreatePage
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class CreatePage : Page
+    {
+        private PFNotification newObject = new PFNotification();
+
+        public CreatePage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private async void Create_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(Title_tb.Text))
+            {
+
+                var dialog = new MessageDialog("The field is empty");
+                await dialog.ShowAsync();
+                return;
+            }
+            else if (string.IsNullOrEmpty(Description_tb.Text))
+            {
+                var dialog = new MessageDialog("The field is empty");
+                await dialog.ShowAsync();
+                return;
+            }
+
+            var result = await new PFNotificationService ().Create(newObject);
+
+            if (result == System.Net.HttpStatusCode.OK)
+            {
+                new InAppNavigationController().GoBack();
+            }
+
+        }
+
+        private async void PictureSelection_btn_Checked(object sender, RoutedEventArgs e)
+        {
+            PictureSelection_btn.IsChecked = !PictureSelection_btn.IsChecked;
+        }
+
+        private async void PictureSelection_btn_Click(object sender, RoutedEventArgs e)
+        {
+            newObject.PictureN = await (await PickFileHelper.PickImage()).ToByteArrayAsync();
+
+        }
+
+        private void Title_tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            newObject.TitleN = Title_tb.Text;
+        }
+
+        private void Description_tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            newObject.DescriptionN = Description_tb.Text;
+        }
+    }
+}
---
+++ ProFind/Lib/Admin/Views/CRUDPages/Notification/ReadPage/ReadPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Application.Models;
+using Application.Services;
+using ProFind.Lib.Admin.Controllers;
+using ProFind.Lib.Admin.Views.CRUDPages.Notification.UpdatePage;
+using ProFind.Lib.Admin.Views.Project_CRUD;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProFind.Lib.Admin.Views.CRUDPages.Notification.ReadPage
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class ReadPage : Page
+    {
+        public ReadPage()
+        {
+            this.InitializeComponent();
+
+            InitializeData();
+        }
+
+        private async void InitializeData()
+        {
+            NotificationListView.ItemsSource = await new PFNotificationService().ListObjectAsync();
+        }
+
+        private void NotificationListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var notification = e.ClickedItem as PFNotification;
+
+            new InAppNavigationController().NavigateTo(typeof(UpdatePage.UpdatePage), notification);
+        }
+
+        private void Add_btn_Click(object sender, RoutedEventArgs e)
+        {
+            new InAppNavigationController().NavigateTo(typeof(CreatePage.CreatePage));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new InAppNavigationController().NavigateTo(typeof(CreatePage.CreatePage));
+        }
+    }
+}
