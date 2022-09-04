441a5e9bc8dcf987a103ad686638c2786955739b
Susan Castillo <susancasoli@gmail.com>
2022-09-03 22:56:53 -0600

    Solo falta proposal tanto de clients como de professionals.

--- ProFind/Lib/ClientNS/Views/CRUDPages/ProjectNS/ReadPage/ReadPage.xaml.cs
+++ ProFind/Lib/ClientNS/Views/CRUDPages/ProjectNS/ReadPage/ReadPage.xaml.cs
@@ -17,7 +17,7 @@ using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace ProFind.Lib.ProfessionalNS.Views.CRUDPage.ProjectNS.ReadPage
+namespace ProFind.Lib.ClientNS.Views.CRUDPages.ProjectNS.ReadPage
 {
     /// <summary>
     /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
