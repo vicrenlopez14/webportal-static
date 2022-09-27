4ff22f0b66c0a94b196d58e64fff7552dcc1751e
Daniel Alessandro Morales Sandoval <20190652@ricaldone.edu.sv>
2022-09-27 10:08:10 -0600

    resuelto

--- ProFind/Lib/AdminNS/Views/CRUDPages/ProjectNS/ReadPage/ReadPage.xaml.cs
+++ ProFind/Lib/AdminNS/Views/CRUDPages/ProjectNS/ReadPage/ReadPage.xaml.cs
@@ -38,14 +38,44 @@ namespace ProFind.Lib.AdminNS.Views.CRUDPages.ProjectNS.ReadPage
             new InAppNavigationController().NavigateTo(typeof(UpdatePage.Update_Project), project);
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private async void SearchBox_QueryChanged(SearchBox sender, SearchBoxQueryChangedEventArgs args)
+        {
+            // Projects in which professional is related
+            var projects = await APIConnection.GetConnection.GetProjectsAsync();
+            projects = projects.Where(x => x.TitlePj.ToLower().Contains(args.QueryText.ToLower())).ToList();
+
+            ProjectsListView.ItemsSource = projects;
+        }
+
+       
+
+        private async void Update_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (ProjectsListView.SelectedItem != null)
+            {
+                var selectedProject = ProjectsListView.SelectedItem as Project;
+                new InAppNavigationController().NavigateTo(typeof(Lib.AdminNS.Views.CRUDPages.ProjectNS.UpdatePage.Update_Project), selectedProject);
+            }
+            else
+            {
+                // Validation content dialog
+                var dialog = new MessageDialog("You have to select a Project.");
+                await dialog.ShowAsync();
+
+            }
+        }
+
+        private void ProjectsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
 
-        
+        private void ProjectsListView_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
 
-        private async void Delete_Click_1(object sender, RoutedEventArgs e)
+        }
+
+        private async void Delete_Click_2(object sender, RoutedEventArgs e)
         {
             try
             {
@@ -72,45 +102,6 @@ namespace ProFind.Lib.AdminNS.Views.CRUDPages.ProjectNS.ReadPage
             {
                 InitializeData();
             }
-        }
-
-        private async void SearchBox_QueryChanged(SearchBox sender, SearchBoxQueryChangedEventArgs args)
-        {
-            // Projects in which professional is related
-            var projects = await APIConnection.GetConnection.GetProjectsAsync();
-            projects = projects.Where(x => x.TitlePj.ToLower().Contains(args.QueryText.ToLower())).ToList();
-
-            ProjectsListView.ItemsSource = projects;
-        }
-
-        private void Add_Click_1(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private async void Update_Click_1(object sender, RoutedEventArgs e)
-        {
-            if (ProjectsListView.SelectedItem != null)
-            {
-                var selectedProject = ProjectsListView.SelectedItem as Project;
-                new InAppNavigationController().NavigateTo(typeof(Lib.AdminNS.Views.CRUDPages.ProjectNS.UpdatePage.Update_Project), selectedProject);
-            }
-            else
-            {
-                // Validation content dialog
-                var dialog = new MessageDialog("You have to select a Project.");
-                await dialog.ShowAsync();
-
-            }
-        }
-
-        private void ProjectsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void ProjectsListView_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
-        {
-
         }
     }
 }
