4b16cb5ccb66611db6916a05305d5984af2bd3ab
Daniel Alessandro Morales Sandoval <20190652@ricaldone.edu.sv>
2022-09-27 21:23:44 -0600

    Complete CLient

--- ProFind/Lib/ClientNS/Views/CRUDPages/ProjectNS/ReadPage/ReadPage.xaml.cs
+++ ProFind/Lib/ClientNS/Views/CRUDPages/ProjectNS/ReadPage/ReadPage.xaml.cs
@@ -7,6 +7,8 @@ using ProFind.Lib.Global.Controllers;
 using ProFind.Lib.ClientNS.Controllers;
 using System.Collections.Generic;
 using System.Linq;
+using Windows.UI.Popups;
+using System;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -54,6 +56,52 @@ namespace ProFind.Lib.ClientNS.Views.CRUDPages.ProjectNS.ReadPage
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
            
+        }
+
+        private async void Add_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            if (AdminsListView.SelectedItem != null)
+            {
+                var dialog = new MessageDialog("You have accepted the project.");
+                await dialog.ShowAsync();
+            }
+            else
+            {
+
+                var dialog = new MessageDialog("You have to select a Project.");
+                await dialog.ShowAsync();
+            }
+        }
+
+        private async void Decline_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                var selectedProject = AdminsListView.SelectedItem as Project;
+                await APIConnection.GetConnection.DeleteProjectAsync(selectedProject.IdPj);
+
+                var dialog = new MessageDialog("Project deleted successfully.");
+                await dialog.ShowAsync();
+            }
+            catch (ProFindServicesException ex)
+            {
+                if (ex.StatusCode >= 200 && ex.StatusCode <= 205)
+                {
+                    var dialog = new MessageDialog("Project deleted successfully.");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    var dialog = new MessageDialog("You have to select an Project.");
+                    await dialog.ShowAsync();
+                }
+            }
+            finally
+            {
+                InitializeData();
+            }
         }
     }
 }
