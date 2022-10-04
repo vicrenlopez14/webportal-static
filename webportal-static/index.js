97e691f31d5d1025e878e2d9e1a7f43629e83074
Víctor López <vicren.lopez@outlook.com>
2022-10-04 11:57:55 -0600

    Fixed css and things

--- webportal-static/index.js
+++ webportal-static/index.js
@@ -4,19 +4,19 @@ const port = process.env.PORT || 3000;
 
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'public/Español')));
-app.use(express.static(path.join(__dirname, 'public/Inglés')));
+app.use(express.static(path.join(__dirname, 'public/')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/Inglés/index.html"));
+    res.redirect('/Inglés/index.html');
+    // res.sendFile(path.join(__dirname, "./public/Inglés/index.html"));
 });
 
 app.get('/es', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/Español/Español.html"));
+    res.redirect('/Español/Español.html');
 });
 
 app.get('/en', (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/Inglés/index.html"));
+    res.redirect('/Inglés/index.html');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`ProFind static page listening on port ${port}!`));
